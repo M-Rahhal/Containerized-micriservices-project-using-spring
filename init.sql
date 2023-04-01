74051a0b7c80860b6e73e1906350728b3940ae79
Mohammad Rahhal <99293726+M-Rahhal@users.noreply.github.com>
2023-04-01 15:05:45 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -3,6 +3,6 @@ USE docker;
 CREATE TABLE user(id int , username varchar(50) , password varchar(50));
 CREATE TABLE grades(student_name varchar(50) , grade double);
 CREATE TABLE statistics(id int , min double , max double , avg double);
-INSERT INTO user VALUES (1 , "mohammad" , "mar2001");
+INSERT INTO user VALUES (1 , "user" , "user");
 
 
